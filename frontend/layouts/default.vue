e62d7eea5aad5cfac0059725e932e6498d936078
peet5228 <ppeet5228@gmail.com>
2026-02-02 12:32:52 +0700

    toel

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,16 +1,17 @@
 <template>
+
   <v-app>
-    <v-app-bar color="#7d0c14" flat>
+    <v-app-bar color="#7d0c14">
       <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-toolbar-title>NTC Evaluation System</v-toolbar-title>
       <v-spacer />
-      <NuxtLink to="/theme" class="mr-4">Theme</NuxtLink>
+      <p class="text-center">ผู้ใช้งาน : {{ user.first_name }} {{ user.last_name }} <br> {{ user.role }}</p>&nbsp;&nbsp;
       <!-- โปรไฟล์ -->
-      <v-btn icon="mdi-account-circle" variant="text" />
+      <v-btn icon="mdi-logout" variant="text" @click="logout" />&nbsp;&nbsp;
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" width="260" color="#404040">
-      <v-list density="comfortable" >
+    <v-navigation-drawer v-model="drawer" color="#404040" :temporary="isMobile" :permanent="!isMobile">
+      <v-list>
         <v-list-item v-for="item in navitem" :key="item.title" :to="item.to">
             <v-list-item-title>
                 {{ item.title }}
@@ -26,12 +27,51 @@
       <v-footer app class="text-caption justify-center">© 2025 NTC Evaluation System</v-footer>
     </v-main>
   </v-app>
+
 </template>
 
-<script setup>
+<script setup lang="ts" >
+import axios from 'axios';
+import { useDisplay } from 'vuetify';
+const {mdAndDown} = useDisplay()
+const isMobile = computed(() => mdAndDown.value)
+const drawer = ref(true)
+const user = ref<any>({})
 
+const logout = async () =>{
+    if(!confirm('ต้องการออกจากระบบใช่มั้ย'))return
+    localStorage.removeItem('token')
+    navigateTo('/',{replace:true})
+}
+
+const roles = [
+    //staff
+    {title:'หน้าหลัก',to:'/Staff/',role:'ฝ่ายบุคลากร'},
+
+    //commit
+    {title:'รายชื่อผู้รับการประเมิน',to:'/Staff/',role:'กรรมการประเมิน'},
+
+    //eva
+    {title:'หน้าหลัก',to:'/Staff/',role:'ผู้รับการประเมินผล'},
+
+]
+
+const navitem = computed(() => roles.filter((item) => item.role.includes(user.value.role)))
+
+const fetchUser = async () =>{
+    const token = localStorage.getItem('token')
+    if(!token){
+        return await navigateTo('/',{replace:true})
+    }
+    try{
+        const res = await axios.get(``)
+    }catch(err){
+        console.error('Error GET User',err)
+    }
+}
+onMounted(fetchUser())
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
