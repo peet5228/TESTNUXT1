5b533bdd13a99d3e0ccef6e7ff49c52ddf97e3a8
peet5228 <ppeet5228@gmail.com>
2026-02-02 11:19:08 +0700

    update

--- frontend/.nuxt/dist/server/client.manifest.mjs
+++ frontend/.nuxt/dist/server/client.manifest.mjs
@@ -1,4 +1,4 @@
 
-import { viteNodeFetch } from "file:///C:/mon/frontend/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
+import { viteNodeFetch } from "file:///app/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
 export default () => viteNodeFetch.getManifest()
     
--- frontend/.nuxt/types/nitro-imports.d.ts
+++ frontend/.nuxt/types/nitro-imports.d.ts
@@ -145,5 +145,5 @@ export { getRouteRules } from 'nitropack/runtime/internal/route-rules';
 export { useEvent } from 'nitropack/runtime/internal/context';
 export { defineTask, runTask } from 'nitropack/runtime/internal/task';
 export { defineNitroErrorHandler } from 'nitropack/runtime/internal/error/utils';
-export { buildAssetsURL as __buildAssetsURL, publicAssetsURL as __publicAssetsURL } from 'C:/mon/frontend/node_modules/@nuxt/nitro-server/dist/runtime/utils/paths';
-export { defineAppConfig } from 'C:/mon/frontend/node_modules/@nuxt/nitro-server/dist/runtime/utils/config';
+export { buildAssetsURL as __buildAssetsURL, publicAssetsURL as __publicAssetsURL } from '/app/node_modules/@nuxt/nitro-server/dist/runtime/utils/paths';
+export { defineAppConfig } from '/app/node_modules/@nuxt/nitro-server/dist/runtime/utils/config';
